4ab9b51a9dfbd898e0025f2e3fd35f8de62e5cfd
nhodges <hodges27@outlook.com>
2022-07-24 03:44:39 -0400

    lovelife

---
+++ src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled, { css } from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: red;
+`;
+
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+
+const Logo = styled(Link)`
+    ${NavLink}
+    font-style: italic;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div`
+display: flex;
+align-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+`;
+
+const NavBtn = styled.div`
+display: flex;
+align-items: center;
+margin-right: 20px;
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <Logo to='/'>Life</Logo>
+        <MenuBars />
+        <NavMenu>
+            {menuData.map((item, index) => (
+                <NavMenuLinks to={item.link} key={index}>
+                    {item.title}
+                </NavMenuLinks>
+            ))}
+        </NavMenu>
+        <NavBtn>
+            <Button to="/contact">Contact Us</Button>
+        </NavBtn>
+        <h1>Navbar</h1>
+    </Nav>
+  )
+}
+
+export default Navbar;
