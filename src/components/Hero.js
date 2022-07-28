f043adfa5952994818f027c1e1cd75037bbd8c97
nhodges <hodges27@outlook.com>
2022-07-27 21:48:45 -0400

    hero updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,31 +5,71 @@ import { IoMdArrowRoundForward } from 'react-icons/io';
 import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
 
 const HeroSection = styled.section`
-    height: 100vh;
-    max-height: 1100px;
-    position: relative;
-    overflow: hidden;
+  height: 100vh;
+  max-height: 1100px;
+  position: relative;
+  overflow: hidden;
 `;
 
 const HeroWrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-    position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
 `;
 
-const HeroSlide = styled.div``;
-const HeroSlider = styled.div``;
-const HeroImage = styled.img``;
+const HeroSlide = styled.div`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
+
+const HeroSlider = styled.div`
+  posiiton: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(
+      0deg, 
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 50%,
+      rgba(0, 0, 0, 0.6) 100%,
+    )
+  }
+`;
+
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+`;
 const HeroContent = styled.div``;
 const Arrow = styled(IoMdArrowRoundForward)``;
 
 const SliderButtons = styled.div`
   position: absolute;
-  button: 50px;
+  bottom: 50px;
   right: 50px;
   display: flex;
   z-index: 10;
@@ -39,6 +79,7 @@ const arrowButtons = css`
   width: 50px;
   height: 50px;
   color: #fff;
+  cursor: pointer;
   background: #000d1a;
   border-radius: 50px;
   padding: 10px;
